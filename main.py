af9da85767b5045e8f7c5abd5731e501cc1f1a77
BolajiOlajide <bolaji.olajide@andela.com>
2019-02-18 01:08:06 +0100

    forms and user input

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from flask import Flask, render_template, url_for
+from flask import (
+    Flask, render_template, flash, redirect, url_for
+)
+
+from forms import RegistrationForm, LoginForm
 
 
 app = Flask(__name__)
-
+# you can generate secrets in Python3 using the secrets module
+# secrets.token_hex(16)
+app.config['SECRET_KEY'] = '27d2fce2529d42dff3ee87337675092d'
 
 posts = [
     {
@@ -35,6 +41,34 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title="About")
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', form=form, title='Register')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if (
+            form.email.data == 'admin@blog.com'
+            and
+            form.password.data == 'password'
+        ):
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash(
+                'Login Unsuccessful. Please check username and password!',
+                'danger'
+            )
+    return render_template('login.html', form=form, title='Log In')
 
 
 if __name__ == "__main__":
