5dd8f84079b4ff23622d2a6b8472820b311d28ea
BolajiOlajide <bolaji.olajide@andela.com>
2019-02-19 22:01:40 +0100

    user account and profile picture

--- app/routes.py
+++ app/routes.py
@@ -7,7 +7,10 @@ from flask_login import (
 
 from app import app, db, bcrypt
 from app.models import User  # noqa: E402
-from app.forms import RegistrationForm, LoginForm
+from app.forms import (
+    RegistrationForm, LoginForm, UpdateAccountForm
+)
+from app.utils import save_picture
 
 
 posts = [
@@ -94,7 +97,26 @@ def logout():
     return redirect(url_for('home'))
 
 
-@app.route('/account', methods=['GET'])
+@app.route('/account', methods=['GET', 'POST'])
 @login_required
 def user_account():
-    return render_template('account.html', title='Account')
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.add(current_user)
+        db.session.commit()
+        flash('Your account has been updated', 'success')
+        return redirect(url_for('user_account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for(
+        'static', filename='pictures/' + current_user.image_file)
+    return render_template(
+        'account.html', title='Account', image_file=image_file,
+        form=form
+    )
